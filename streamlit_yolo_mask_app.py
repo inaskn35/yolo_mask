1de4e49e4e2254a0a84cc13dea34f0cbe61cb70f
inaskn35 <ina.skn35@gmail.com>
2026-08-25 09:43:56 +0900

    Update streamlit_yolo_mask_app.py

--- streamlit_yolo_mask_app.py
+++ streamlit_yolo_mask_app.py
@@ -83,7 +83,7 @@ elif mode == "동영상":
             stframe.image(cv2.cvtColor(result_bgr, cv2.COLOR_BGR2RGB), channels="RGB", use_container_width=True)
             # 잠시 대기 - 너무 빠른 루프 방지
             #if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+               # break
 
         cap.release()
 
